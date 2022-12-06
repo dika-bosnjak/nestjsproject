22a2e9fe8f504311f7ea5c5826424e172bce64bc
Hamza Starcevic <starcevic.hamza.21@size.ba>
2022-12-06 16:34:28 +0100

    Some more work on the payment

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,21 +1,42 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 
 @Controller('payment')
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
 
-  @Get('one-time-payment')
-  async oneTimePayment(@Res() res) {
+  @Post('one-time-payment')
+  async oneTimePayment(@Req() req, @Res() res) {
+    const documentId = req.body.docId;
+    console.log('documentId', documentId);
     const session =
-      await this.paymentService.oneTimeDocumentGenerationPayment();
+      await this.paymentService.oneTimeDocumentGenerationPayment(
+        documentId,
+      );
 
-    return res.redirect(303, session.url);
+    return res.send(session.url);
   }
 
   @Get('success')
-  async success(@Res() res) {
-    return res.send('Success');
+  async success(@Res() res, @Req() req) {
+    try {
+      const paidStatus =
+        await this.paymentService.checkSessionStatus(
+          req.query.session_id,
+        );
+      if (paidStatus) {
+        res.status(200);
+        return res.send('Payment successful');
+      }
+    } catch (error) {
+      return res.send('An error in your payment occurred.');
+    }
   }
 
   @Get('cancel')
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -9,20 +9,41 @@ export class PaymentService {
     },
   );
 
-  async oneTimeDocumentGenerationPayment() {
-    const session =
-      await this.stripe.checkout.sessions.create({
-        mode: 'payment',
-        line_items: [
-          {
-            price: 'price_1MAbhsGRCS0CQqRaDjbLLzTc',
-            quantity: 1,
-          },
-        ],
-        success_url:
-          'http://localhost:3000/payment/success',
-        cancel_url: 'http://localhost:3000/payment/cancel',
-      });
-    return session;
+  async oneTimeDocumentGenerationPayment(docId) {
+    try {
+      const session =
+        await this.stripe.checkout.sessions.create({
+          mode: 'payment',
+          line_items: [
+            {
+              price: 'price_1MAbhsGRCS0CQqRaDjbLLzTc',
+              quantity: 1,
+            },
+          ],
+          success_url: `http://localhost:3000/payment/success?session_id={CHECKOUT_SESSION_ID}&docId=${docId}`,
+          cancel_url:
+            'http://localhost:3000/payment/cancel',
+        });
+
+      return session;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async checkSessionStatus(sessionId: string) {
+    try {
+      const session =
+        await this.stripe.checkout.sessions.retrieve(
+          sessionId,
+        );
+      if (session.payment_status === 'paid') {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
   }
 }
